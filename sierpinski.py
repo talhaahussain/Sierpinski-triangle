014803deb9ae1d308d971921e5ba4322690d8d6d
talhaahussain <talhaahussain@gmail.com>
2023-09-04 15:34:26 +0100

    Added generateTriangle() function to sierpinski.py
    Specified vertices for the equilateral triangle
    generateTriangle() draws the 3 initial vertices for the triangle

--- sierpinski.py
+++ sierpinski.py
@@ -4,4 +4,16 @@ import random
 t = turtle.Turtle()
 t.width(5)
 t.shape("circle")
-t.penup()    
+t.penup()
+
+vertices = [(0, 250), (-216.506, -125), (216.506, -125)]
+
+def generateTriangle(t, vertices):
+    for v in vertices:
+        t.goto(v)
+        t.dot()
+        t.penup()
+    return
+
+
+generateTriangle(t, vertices)    
